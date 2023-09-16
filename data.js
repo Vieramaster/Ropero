a6269f65e5558024c1c99f8396682949b9798126
martin viera <118699912+Vieramaster@users.noreply.github.com>
2023-09-16 17:00:56 -0300

    actualizando Js

--- data.js
+++ data.js
@@ -1,93 +1,148 @@
 /*Modal cuenta*/
-const BtnOpenAcc = document.querySelector("#btn-open-acc")
-const modalAcc = document.querySelector("#modal-acc")
+const BtnOpenAcc = document.querySelector("#btn-open-acc");
+const modalAcc = document.querySelector("#modal-acc");
 
-
-BtnOpenAcc.addEventListener("click", ()=> {
-    modalAcc.showModal()
-    
-})
-window.onclick = function(event){
-    if (event.target == modalAcc)
-    modalAcc.close()
-} 
+BtnOpenAcc.addEventListener("click", () => {
+  modalAcc.showModal();
+});
+window.onclick = function (event) {
+  if (event.target == modalAcc) modalAcc.close();
+};
 
 /* SUSTRAER LOS ARRAYS*/
 
-
-
-function datosTop(){
-    return fetch('./database.json')  // reemplaza esto con la ruta a tu archivo JSON
-    .then(response => response.json())
-    .then(data => data.top)
-    .then(data => data.abri)
-    .catch(error => console.error('Error:', error));
-
+function datosRopa() {
+  return fetch("./database.json")
+    .then((response) => response.json())
+    .catch(console.error);
 }
 
+const btnTop = document.querySelector("#top");
+const boxTop = document.querySelector(".box2__top");
+const box2 = document.querySelector(".box2");
 
+/*-------------------------- funciones------------------------------*/
+
+function cargarImagenes(imagenes, tipoRopa) {
+  imagenes.forEach((imagen) => {
+    let imagenRuta = document.createElement("img");
+    imagenRuta.src = imagen;
+    let imagenDiv = document.createElement("div");
+    imagenDiv.appendChild(imagenRuta);
+    box2.appendChild(imagenDiv);
+
+    imagenRuta.style.width = "80%";
+    imagenDiv.classList.add(`block__${tipoRopa}`);
+    if (tipoRopa === "top" || tipoRopa === "abrigo") {
+      imagenDiv.classList.add("block1__top");
+    } else if (tipoRopa === "cuello") {
+      imagenDiv.classList.add("block2__cuello");
+    } else if (tipoRopa === "cinturon") {
+      imagenDiv.classList.add("block2__cinturon");
+    }else if (tipoRopa === "reloj"){
+      imagenDiv.classList.add ("block2__reloj"); 
+    } else if (tipoRopa === "pantalon"){
+      imagenDiv.classList.add("block3__pantalon")
+    } else if (tipoRopa === "calzado")
+      imagenDiv.classList.add("block3__calzado")
+    
+      
+  } 
+  );
+}
 
 /*paso 1, agregar las img*/
 
+function cargarTop() {
+  box2.innerHTML = "";
+  datosRopa().then((datos) => {
+    let imagenes = datos.top.map((item) => item.imagen);
+    cargarImagenes(imagenes, "top");
+  });
+}
+btnTop.addEventListener("click", cargarTop);
 
-const btnTop = document.querySelector("#top")
-const boxTop = document.querySelector(".box2__top")
-const box2 = document.querySelector(".box2")
-
-let isClicked = false;
-
-function cargarTop (){
-    
-        if (!isClicked) {
-            datosTop().then(top => {
-                let imagenes = top.map(item => item.imagen);  
-    
-                imagenes.forEach(imagen => {
-                    let imagenRuta= document.createElement("img");
-                    imagenRuta.src = imagen;
-                    let imagenDiv = document.createElement("div")
-                    imagenDiv.appendChild(imagenRuta)
-                    box2.appendChild(imagenDiv);
-                    imagenDiv.classList.add ("block1__top")
-                    imagenDiv.style.transition = "0.3s all ease"
-                    imagenRuta.style.width = "80%"
-                });
-            });
-            isClicked = true;
-        }
-    };
-
-btnTop.addEventListener("click", cargarTop)
 /*---------------------------------------------------*/
-const btnAbrigo = document.querySelector("#abrigo")
 
-function datosAbrigo(){
-    return fetch('./database.json')  // reemplaza esto con la ruta a tu archivo JSON
-    .then(response => response.json())
-    .then(data => data.abrigo)
-    .catch(error => console.error('Error:', error));
+const btnAbrigo = document.querySelector("#abrigo");
 
+function cargarAbrigo() {
+  box2.innerHTML = "";
+  datosRopa().then((datos) => {
+    let imagenes = datos.abrigo.map((item) => item.imagen);
+    cargarImagenes(imagenes, "abrigo");
+  });
 }
 
-function cargarAbrigo (){
-    
-    if (!isClicked) {
-        datosAbrigo().then(abrigo => {
-            let imagenes = abrigo.map(item => item.imagen);  
+btnAbrigo.addEventListener("click", cargarAbrigo);
 
-            imagenes.forEach(imagen => {
-                let imagenRuta= document.createElement("img");
-                imagenRuta.src = imagen;
-                let imagenDiv = document.createElement("div")
-                imagenDiv.appendChild(imagenRuta)
-                box2.appendChild(imagenDiv);
-                imagenDiv.classList.add ("block1__top")
-                
-                imagenRuta.style.width = "80%"
-            });
-        });
-        isClicked = true;
-    }
-};
+/*---------------------------------------------------*/
 
-btnAbrigo.addEventListener("click", cargarAbrigo)
+const btnCuello = document.querySelector("#cuello");
+
+function cargarCuello() {
+  box2.innerHTML = "";
+  datosRopa().then((datos) => {
+    let imagenes = datos.cuello.map((item) => item.imagen);
+    cargarImagenes(imagenes, "cuello");
+  });
+}
+
+btnCuello.addEventListener("click", cargarCuello);
+
+/*---------------------------------------------------*/
+
+const btnCinturon = document.querySelector(".block2__cinturon");
+
+function cargarCinturon() {
+  box2.innerHTML = "";
+  datosRopa().then((datos) => {
+    let imagenes = datos.cinturon.map((item) => item.imagen);
+    cargarImagenes(imagenes, "cinturon");
+  });
+}
+
+btnCinturon.addEventListener("click", cargarCinturon);
+
+
+/*---------------------------------------------------*/
+
+const btnReloj = document.querySelector(".block2__reloj")
+
+function cargarReloj() {
+  box2.innerHTML = "";
+  datosRopa().then((datos) => {
+    let imagenes = datos.reloj.map((item) => item.imagen);
+    cargarImagenes(imagenes, "reloj")
+  })
+}
+
+btnReloj.addEventListener("click", cargarReloj)
+
+/*---------------------------------------------------*/
+
+const btnPantalon = document.querySelector(".block3__pantalon")
+
+function cargarPantalon(){
+  box2.innerHTML = ""
+  datosRopa().then((datos) =>{
+    let imagenes = datos.pantalon.map((item) => item.imagen)
+    cargarImagenes(imagenes, "pantalon")
+  })
+}
+
+btnPantalon.addEventListener("click", cargarPantalon)
+
+/*---------------------------------------------------*/
+
+const btnCalzado = document.querySelector(".block3__calzado")
+
+function cargarCalzado(){
+  box2.innerHTML = ""
+  datosRopa().then((datos) =>{
+    let imagenes = datos.calzado.map((item)=> item.imagen)
+    cargarImagenes(imagenes, "calzado")
+  })
+}
+
+btnCalzado.addEventListener("click", cargarCalzado)
